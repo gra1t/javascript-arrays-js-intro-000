3c572bb9f62a6c56688bc6c50f9d433ab419685d
granit <graniitkastrati@gmail.com>
2019-10-07 17:22:12 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,3 +1,5 @@
 var chocolateBars = [ "snickers", "hundred grand", "kitkat", "skittles"]
-var addElementToBeginningOfArray =[]
-addElementToBeginningOfArray.unshift("foo")
+function addElementToBeginningOfArray(array,element){
+  array.unshift("foo")
+}
+
