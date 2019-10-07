8fb297969a5ee98af7c3b20dbf446c4055f5b7dc
granit <graniitkastrati@gmail.com>
2019-10-07 17:49:46 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -4,7 +4,7 @@ function addElementToBeginningOfArray(array, element) {
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
-  array.unshift(element)
+  array=[element,...array]
   return array
 }
 
