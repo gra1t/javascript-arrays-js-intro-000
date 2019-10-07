713668d79c7254e77a0017191b2adaab06daad92
granit <graniitkastrati@gmail.com>
2019-10-07 17:41:46 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,5 +1,10 @@
 var chocolateBars = [ "snickers", "hundred grand", "kitkat", "skittles"]
 function addElementToBeginningOfArray(array,element){
-  array.unshift()
+  ('element',...array)
+  return array
 }
+function destructivelyRemoveElementFromBeginningOfArray (array,element){
+  array=['element',...array]
+  return array
 
+}
